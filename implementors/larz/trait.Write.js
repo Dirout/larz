4702c3f1efe03e519d2fd1e0fb835b4cc23a033d
larz <Dirout@users.noreply.github.com>
2021-11-19 06:35:03 +0000

    Publish documentation to GitHub Pages

---
+++ implementors/larz/trait.Write.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["larz"] = [];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
