0cb7d3266429d8d5f90319bbc50407bfa23f6896
larz <Dirout@users.noreply.github.com>
2022-01-15 21:26:38 +0000

    Publish documentation to GitHub Pages

--- search-index.js
+++ search-index.js
@@ -1,4 +1,4 @@
 var searchIndex = JSON.parse('{\
-"larz":{"doc":"","t":[5,5,5],"n":["compress_archive","extract_archive","write_file"],"q":["larz","",""],"d":["Archive a file or set of files","Decompress an archive","Write a file to the filesystem"],"i":[0,0,0],"f":[[[["argmatches",3]]],[[["argmatches",3]]],[[["str",15]]]],"p":[]}\
+"larz":{"doc":"","t":[5,5],"n":["compress_archive","extract_archive"],"q":["larz",""],"d":["Archive a file or set of files","Decompress an archive"],"i":[0,0],"f":[[[["argmatches",3]]],[[["argmatches",3]]]],"p":[]}\
 }');
 if (window.initSearch) {window.initSearch(searchIndex)};
