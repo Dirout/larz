29547e23167693fd8bdfb8567e76286254d82bc9
larz <Dirout@users.noreply.github.com>
2022-06-02 15:32:36 +0000

    Publish documentation to GitHub Pages

--- search-index.js
+++ search-index.js
@@ -1,4 +1,5 @@
 var searchIndex = JSON.parse('{\
 "larz":{"doc":"","t":[5,5,5,5,5],"n":["compress_archive_memory","compress_archive_streaming","extract_archive_memory","extract_archive_streaming","get_absolute_path"],"q":["larz","","","",""],"d":["Archive a file or set of files using only memory","Archive a file or set of files","Decompress an archive created using only memory","Decompress an archive","Get an absolute, canonical path from a String"],"i":[0,0,0,0,0],"f":[[[["vec",3,[["string",3]]],["string",3]]],[[["vec",3,[["string",3]]],["string",3]]],[[["vec",3,[["string",3]]],["string",3]]],[[["vec",3,[["string",3]]],["string",3]]],[[["string",3]],["pathbuf",3]]],"p":[]}\
 }');
-if (window.initSearch) {window.initSearch(searchIndex)};
+if (typeof window !== 'undefined' && window.initSearch) {window.initSearch(searchIndex)};
+if (typeof exports !== 'undefined') {exports.searchIndex = searchIndex};
--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
-var N = null;var sourcesIndex = {};
+var sourcesIndex = {};
 sourcesIndex["larz"] = {"name":"","files":["lib.rs"]};
 createSourceSidebar();
