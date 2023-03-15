825c43d7fc8a0668d2f7c9c31c5f2a9337cce14a
larz <Dirout@users.noreply.github.com>
2023-03-15 17:21:09 +0000

    Publish documentation to GitHub Pages

--- search-index.js
+++ search-index.js
@@ -1,5 +1,5 @@
 var searchIndex = JSON.parse('{\
-"larz":{"doc":"larz","t":"FFFF","n":["compress_archive_memory","compress_archive_streaming","extract_archive_memory","extract_archive_streaming"],"q":["larz","","",""],"d":["Archive &amp; compress a file or set of files, in memory","Archive &amp; compress a file or set of files","Extract &amp; decompress an existing archive, in memory","Extract &amp; decompress an existing archive"],"i":[0,0,0,0],"f":[[[[2,[1]],1,[4,[3]]]],[[[2,[1]],1,[4,[3]]]],[[[2,[1]],1]],[[[2,[1]],1]]],"p":[[3,"PathBuf"],[3,"Vec"],[3,"BufWriter"],[4,"Option"]]}\
+"larz":{"doc":"larz","t":"FFFF","n":["compress_archive_memory","compress_archive_streaming","extract_archive_memory","extract_archive_streaming"],"q":[[0,"larz"]],"d":["Archive &amp; compress a file or set of files, in memory","Archive &amp; compress a file or set of files","Extract &amp; decompress an existing archive, in memory","Extract &amp; decompress an existing archive"],"i":[0,0,0,0],"f":[[[[2,[1]],1,[4,[3]]]],[[[2,[1]],1,[4,[3]]]],[[[2,[1]],1]],[[[2,[1]],1]]],"c":[],"p":[[3,"PathBuf"],[3,"Vec"],[3,"BufWriter"],[4,"Option"]]}\
 }');
 if (typeof window !== 'undefined' && window.initSearch) {window.initSearch(searchIndex)};
 if (typeof exports !== 'undefined') {exports.searchIndex = searchIndex};
