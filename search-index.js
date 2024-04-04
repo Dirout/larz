17b9657f092cc55f8dc10e581a3d1a85ae1a61b9
larz <Dirout@users.noreply.github.com>
2024-04-04 00:12:11 +0000

    Publish documentation to GitHub Pages

--- search-index.js
+++ search-index.js
@@ -1,5 +1,5 @@
 var searchIndex = new Map(JSON.parse('[\
-["larz",{"doc":"larz","t":"HHHH","n":["compress_archive_memory","compress_archive_streaming","extract_archive_memory","extract_archive_streaming"],"q":[[0,"larz"],[4,"std::path"],[5,"alloc::vec"],[6,"std::io::buffered::bufwriter"],[7,"core::option"],[8,"std::io"]],"d":["Archive &amp; compress a file or set of files, in memory","Archive &amp; compress a file or set of files","Extract &amp; decompress an existing archive, in memory","Extract &amp; decompress an existing archive"],"i":[0,0,0,0],"f":"{{{d{b}}b{h{{f{c}}}}}jl}0{{{d{b}}b}j}0","c":[],"p":[[5,"PathBuf",4],[5,"Vec",5],[5,"BufWriter",6],[6,"Option",7],[1,"unit"],[10,"Write",8]],"b":[]}]\
+["larz",{"t":"HHHH","n":["compress_archive_memory","compress_archive_streaming","extract_archive_memory","extract_archive_streaming"],"q":[[0,"larz"],[4,"std::path"],[5,"alloc::vec"],[6,"std::io::buffered::bufwriter"],[7,"core::option"],[8,"std::io"]],"i":[0,0,0,0],"f":"{{{d{b}}b{h{{f{c}}}}}jl}0{{{d{b}}b}j}0","D":"j","p":[[5,"PathBuf",4],[5,"Vec",5],[5,"BufWriter",6],[6,"Option",7],[1,"unit"],[10,"Write",8]],"b":[],"c":"OjAAAAAAAAA=","e":"OjAAAAAAAAA="}]\
 ]'));
 if (typeof exports !== 'undefined') exports.searchIndex = searchIndex;
 else if (window.initSearch) window.initSearch(searchIndex);
