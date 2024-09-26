7f3758d00d084aeea8401bf864849b0d29b036eb
larz <Dirout@users.noreply.github.com>
2024-09-26 19:17:13 +0000

    Publish documentation to GitHub Pages

--- crates.js
+++ crates.js
@@ -1,1 +1,2 @@
 window.ALL_CRATES = ["larz"];
+//{"start":21,"fragment_lengths":[6]}
--- search-index.js
+++ search-index.js
@@ -1,5 +1,4 @@
-var searchIndex = new Map(JSON.parse('[\
-["larz",{"t":"HHHH","n":["compress_archive_memory","compress_archive_streaming","extract_archive_memory","extract_archive_streaming"],"q":[[0,"larz"],[4,"std::path"],[5,"alloc::vec"],[6,"std::io::buffered::bufwriter"],[7,"core::option"],[8,"std::io"]],"i":[0,0,0,0],"f":"{{{d{b}}b{l{{j{f{h{c}}}}}}}nA`}0{{{d{b}}b}n}0","D":"j","p":[[5,"PathBuf",4],[5,"Vec",5],[0,"mut"],[5,"BufWriter",6],[1,"reference"],[6,"Option",7],[1,"unit"],[10,"Write",8]],"r":[],"b":[],"c":"OjAAAAAAAAA=","e":"OjAAAAAAAAA="}]\
-]'));
+var searchIndex = new Map(JSON.parse('[["larz",{"t":"HHHH","n":["compress_archive_memory","compress_archive_streaming","extract_archive_memory","extract_archive_streaming"],"q":[[0,"larz"],[4,"std::path"],[5,"alloc::vec"],[6,"std::io::buffered::bufwriter"],[7,"core::option"],[8,"std::io"]],"i":"````","f":"{{{d{b}}b{l{{j{f{h{c}}}}}}}nA`}0{{{d{b}}b}n}0","D":"j","p":[[5,"PathBuf",4],[5,"Vec",5],[0,"mut"],[5,"BufWriter",6],[1,"reference"],[6,"Option",7],[1,"unit"],[10,"Write",8]],"r":[],"b":[],"c":"OjAAAAAAAAA=","e":"OjAAAAAAAAA="}]]'));
 if (typeof exports !== 'undefined') exports.searchIndex = searchIndex;
 else if (window.initSearch) window.initSearch(searchIndex);
+//{"start":39,"fragment_lengths":[495]}
