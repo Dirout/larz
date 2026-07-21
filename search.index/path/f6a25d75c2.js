8ef2fbb42e8228772811e442908f1df4adec92f8
larz <Dirout@users.noreply.github.com>
2026-07-21 03:59:58 +0000

    Publish documentation to GitHub Pages

---
+++ search.index/path/f6a25d75c2.js
@@ -0,0 +1,1 @@
+rd_("f[1,\"\"]Am[5,\"alloc::vec\",\"alloc::vec\"]f[0,\"\"]f[3,\"\"]3Bh[10,\"core::io::write\",\"core::io::write\"]Ba[6,\"core::option\",\"core::option\"]Ak[5,\"std::path\",\"std::path\"]Da[5,\"std::io::buffered::bufwriter\",\"std::io::buffered::bufwriter\"]7")
